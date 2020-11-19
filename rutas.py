cf5830f72588260cbd299184b0f7a75137dc331d
LDRCTEC19 <ldrc161997@gmail.com>
2020-11-18 23:40:41 -0600

    respuesta de problema semiresuelto

--- rutas.py
+++ rutas.py
@@ -1,2 +1,12 @@
-def encontrar_ruta(C):
-    raise NotImplementedError()
+def encontrarRuta(C):
+    m = auxiliar(C,1,2)
+    if m==[]:
+        return []
+    else: 
+        for i in range(0, len(m)):
+            for j in range (0, len(m[0])):
+                if matriz[i][j] == 'T':
+                    matriz[i][j] = 1
+                else:
+                    matriz[i][j] = 0
+    return matriz
